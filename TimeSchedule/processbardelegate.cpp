6d54e294c6bd1a04f4b6fe1d3c2eb36da8bde386
SteakingCoder <1941499609@qq.com>
2017-12-09 18:39:32 +0800

    实现基本的表格内进度条

---
+++ TimeSchedule/processbardelegate.cpp
@@ -0,0 +1,6 @@
+#include "processbardelegate.h"
+
+ProcessBarDelegate::ProcessBarDelegate()
+{
+
+}
---
+++ TimeSchedule/processbardelegate.h
@@ -0,0 +1,11 @@
+#ifndef PROCESSBARDELEGATE_H
+#define PROCESSBARDELEGATE_H
+
+
+class ProcessBarDelegate : public QItemDelegate
+{
+public:
+    ProcessBarDelegate();
+};
+
+#endif // PROCESSBARDELEGATE_H
